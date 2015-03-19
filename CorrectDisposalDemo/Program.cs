a1933d1ffdec1ce3543c445db8f726efa1b8c05c
RichardCGrimmer <richard.grimmer@w2globaldata.com>
2015-03-19 14:49:07 +0000

    Implemented a leak

--- CorrectDisposalDemo/Program.cs
+++ CorrectDisposalDemo/Program.cs
@@ -7,59 +7,75 @@ using System.Threading.Tasks;
 
 namespace CorrectDisposalDemo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ConsoleKeyInfo s = Console.ReadKey();
-            do
-            {
-                switch (s.Key)
-                {
-                    case ConsoleKey.B:
-                        UseBaseLogger();
-                        break;
-                    case ConsoleKey.N:
-                        UseNonDisposedLogger();
-                        break;
-                    case ConsoleKey.D:
-                        UseDisposedLogger();
-                        break;
-                } 
-            
-                // Log Memory usage...
-                var currentProcess = Process.GetCurrentProcess();
-                string message = string.Format("Current Memory Usage: {0}", currentProcess.WorkingSet64);
-                Console.WriteLine(message);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConsoleKeyInfo s = Console.ReadKey();
+			do
+			{
+				switch (s.Key)
+				{
+					case ConsoleKey.B:
+						UseBaseLogger();
+						break;
+					case ConsoleKey.N:
+						UseNonDisposedLogger();
+						break;
+					case ConsoleKey.D:
+						UseDisposedLogger();
+						break;
+					case ConsoleKey.O:
+						LeakNonDisposedLogger();
+						break;
+					case ConsoleKey.P:
+						LeakDisposedLogger();
+						break;
+				}
 
-                s = Console.ReadKey();
-            
-            } while (s.Key != ConsoleKey.Q);
+				// Log Memory usage...
+				var currentProcess = Process.GetCurrentProcess();
+				string message = string.Format("Current Memory Usage: {0}", currentProcess.WorkingSet64);
+				Console.WriteLine(message);
 
-            
-        }
+				s = Console.ReadKey();
 
-        private static void UseDisposedLogger()
-        {
-            using (DisposedLogger dLogger = new DisposedLogger())
-            {
-                dLogger.Log("Disposed Logger");
-            }
-        }
+			} while (s.Key != ConsoleKey.Q);
+		}
 
-        private static void UseNonDisposedLogger()
-        {
-            using (NonDisposedLogger ndLogger = new NonDisposedLogger())
-            {
-                ndLogger.Log("Non Disposed Logger");
-            }
-        }
+		private static void LeakNonDisposedLogger()
+		{
+			var logger = new NonDisposedLogger();
+			logger.Log("Dropped out of scope");
+		}
 
-        private static void UseBaseLogger()
-        {
-            // Not disposed of in any way - underlying StreamWriter remains open and so the next call fails since it is holding it despite having dropped out of scope...
-            var logger = new BaseLogger();
-            logger.Log("Base Logger");
-        }
-    }
+		private static void LeakDisposedLogger()
+		{
+			var logger = new DisposedLogger();
+			logger.Log("Dropped out of scope");
+		}
+
+		private static void UseDisposedLogger()
+		{
+			using (DisposedLogger dLogger = new DisposedLogger())
+			{
+				dLogger.Log("Disposed Logger");
+			}
+		}
+
+		private static void UseNonDisposedLogger()
+		{
+			using (NonDisposedLogger ndLogger = new NonDisposedLogger())
+			{
+				ndLogger.Log("Non Disposed Logger");
+			}
+		}
+
+		private static void UseBaseLogger()
+		{
+			// Not disposed of in any way - underlying StreamWriter remains open and so the next call fails since it is holding it despite having dropped out of scope...
+			var logger = new BaseLogger();
+			logger.Log("Base Logger");
+		}
+	}
 }
